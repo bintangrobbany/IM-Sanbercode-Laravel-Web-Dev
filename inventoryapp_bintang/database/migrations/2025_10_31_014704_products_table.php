7857815f4d47a87d86cbd070fa73c2107efb594f
bintangrobbany <bintang.robbany11@gmail.com>
2025-10-31 08:50:37 +0700

    Database Migrations Course

---
+++ inventoryapp_bintang/database/migrations/2025_10_31_014704_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id(); // Primary key, auto_increment
+            $table->string('name', 255);
+            $table->string('image', 255)->nullable();
+            $table->text('description')->nullable();
+            $table->integer('price');
+            $table->integer('stock');
+
+            // Definisi Foreign Key
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
